77db98552a2cfda29f4c0f17ddd937424bedb12e
mariemsafwat20 <100482871+mariemsafwat20@users.noreply.github.com>
2023-10-12 19:48:01 +0200

    Reset Password

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./reset-password.component.scss']
 })
 export class ResetPasswordComponent {
+  isLoading:boolean = false;
+  errorResetPassword:string = ''
 
+  constructor(private _Auth:AuthService,  private _Router:Router){}
+  
+  resetForm:FormGroup = new FormGroup({
+    email:new FormControl(null,[Validators.required,Validators.email]),
+    newPassword:new FormControl(null,[Validators.required]),
+    // password:new FormControl(null,[Validators.required,Validators.pattern('^[A-Z][a-z][0-9]{4,8}')]),
+  })
+
+  resetPassword(resetForm:FormGroup){
+    this.isLoading = true;  
+    console.log(resetForm );
+    this._Auth.resetPassword(this.resetForm.value).subscribe({
+      next:(response)=>{
+        console.log(response);
+        if(response.token){
+          this._Router.navigate(['/signin'])
+        }
+      },
+      error:(err)=>{
+        console.log(err)
+        this.isLoading = false
+        this.errorResetPassword = err.error.message
+      }
+    })
+  }
 }
